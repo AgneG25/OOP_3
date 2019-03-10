1f1c3c9f5b643e1ffcdde80ee4f1dd1c4916eead
AgneG25 <44949532+AgneG25@users.noreply.github.com>
2019-03-10 17:01:42 +0200

    Add files via upload

---
+++ vector/libraries.h
@@ -0,0 +1,33 @@
+#ifndef LIBRARIES_H
+#define LIBRARIES_H
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <limits>
+#include <algorithm>
+#include <functional>
+#include <vector>
+#include <ctime>
+#include <stdlib.h> 
+#include <fstream>
+#include <string.h>
+#include <chrono>
+#include <ctype.h>
+#include <list>
+#include <random>
+#include <stdexcept>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::left;
+using std::setw;
+using std::setprecision;
+using std::fixed;
+using std::vector;
+using std::list;
+using namespace std::chrono;
+
+#endif
---
+++ vector/main.cpp
@@ -0,0 +1,23 @@
+#include "functions.h"
+#include "libraries.h"
+
+int main() {
+
+    auto start = high_resolution_clock::now();
+    int nr = 5; 
+    int longestName, longestSurname;
+    int N = 3;
+    vector<stud> Students;
+    vector<stud> Weak;
+    string file = "kursiokai.txt";
+    //Read(N, Students, longestName, longestSurname, nr);
+    //Read_from_file(Students, longestName, longestSurname, file);
+    //Sort_By_firstName(Students);
+    //Write(Students, longestName, longestSurname);
+    New_Students (Students, longestName, longestSurname, Weak);
+    auto end = high_resolution_clock::now();
+    duration<double> diff = end-start;
+    cout << "Visos programos veikimas uztruko: " << diff.count() << " s/n" << endl;
+
+    return 0;
+}
